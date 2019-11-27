f7a8349d2cf5a5080f275d5cf6148567f52f0ad8
rajkonkret <rajkonkret660@gmail.com>
2019-11-27 13:59:57 +0100

    dodana klasa Car i konstruktor Car

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private String brand;
+    private String model;
+    private int price;
+
+    public Car(String brand, String model, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
 //    Stworzyć doradcę kupna samochodu,
 //    którego celem będzie znalezienie najtańszej oferty spośród kilku dealerów samochodowych.
-//    W tym celu:1.Stworzyć klasę Carzawierającą:-brand-model-price
-//    2.Stworzyćklasę Dealerzawierającą pola:-name-adress-tablicę obiektówCar
+//    W tym celu:1.Stworzyć klasę Car zawierającą:-brand-model-price
+//    2.Stworzyćklasę Dealer zawierającą pola:-name-adress-tablicę obiektów Car
 //    3.Stworzyć klasę BuyingCarAdvisor, która zawierać będzie-tablicę obiektów
 //    Dealer[]-konstruktor przyjmujący Dealer[]-metodę findBestOffer(),
 //    która znajdzie i zwróci najtańszy samochód spośród wszystkich dealerów.
 //    4.W nowej klasie napisać metodę mainw taki sposób by:a.tworzyła przykładowe samochody
 //    b.tworzyła przykładowych dealerów
 //    c.tworzyła obiekt BuyingCarAdvisor
-//    d.wywoływała metodę findBestOffer()e.wypisywała najlepszą ofertę
+//    d.wywoływała metodę findBestOffer()
+//    e.wypisywała najlepszą ofertę
 //    Sugeruję by najtańszym autem okazał się.... Fiat Panda.
 
     public static void main(String[] args) {
