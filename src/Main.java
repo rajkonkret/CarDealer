c1d548f17246ecf0a86ad9e8ab124c34442f074b
rajkonkret <rajkonkret660@gmail.com>
2019-11-27 13:42:50 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+//    Stworzyć doradcę kupna samochodu,
+//    którego celem będzie znalezienie najtańszej oferty spośród kilku dealerów samochodowych.
+//    W tym celu:1.Stworzyć klasę Carzawierającą:-brand-model-price
+//    2.Stworzyćklasę Dealerzawierającą pola:-name-adress-tablicę obiektówCar
+//    3.Stworzyć klasę BuyingCarAdvisor, która zawierać będzie-tablicę obiektów
+//    Dealer[]-konstruktor przyjmujący Dealer[]-metodę findBestOffer(),
+//    która znajdzie i zwróci najtańszy samochód spośród wszystkich dealerów.
+//    4.W nowej klasie napisać metodę mainw taki sposób by:a.tworzyła przykładowe samochody
+//    b.tworzyła przykładowych dealerów
+//    c.tworzyła obiekt BuyingCarAdvisor
+//    d.wywoływała metodę findBestOffer()e.wypisywała najlepszą ofertę
+//    Sugeruję by najtańszym autem okazał się.... Fiat Panda.
+}
